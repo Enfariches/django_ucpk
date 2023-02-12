ac4a2b3e23ca58af4e31c1b8f0ab496f67bb137e
Sergey <sergey.pervukhin@urfu.me>
2023-02-12 14:08:40 +0500

    Ready

--- app1/views.py
+++ app1/views.py
@@ -4,5 +4,131 @@ from django.shortcuts import render
 def index_page(request):
     return render(request, 'index.html')
 
+def first_help_page(request):
+    return render(request, 'Направления обучения/first_help.html')
+
+def occupational_page(request):
+    return render(request, 'Направления обучения/occupational_safety.html')
+
+def professions_page(request):
+    return render(request, 'Направления обучения/professions.html')
+
+def teachers_page(request):
+    return render(request, 'Направления обучения/teachers.html')
+
 def second_page(request):
     return render(request, 'second.html')
+
+def forthypointforty_page(request):
+    return render(request, '404.html')
+
+def news_page(request):
+    return render(request, 'news/news_main.html')
+
+def news_new_site_obr_page(request):
+    return render(request, 'news/news_new_site_obr.html')
+
+def medic_and_student_page(request):
+    return render(request, 'news/medic_and_student.html')
+
+def greep_page(request):
+    return render(request, 'news/greep.html')
+
+def hygienic_page(request):
+    return render(request, 'news/hygienic.html')
+
+def prevention_page(request):
+    return render(request, 'news/prevention.html')
+
+def virus_page(request):
+    return render(request, 'news/virus.html')
+
+def contacts_main_page(request):
+    return render(request, 'contacts/main_contacts.html')
+
+def faq_page(request):
+    return render(request, 'contacts/faq.html')
+
+def appeals_page(request):
+    return render(request, 'contacts/appeals.html')
+
+def comments_page(request):
+    return render(request, 'contacts/comments.html')
+
+def deal_page(request):
+    return render(request, 'contacts/deal.html')
+
+def attestation_page(request):
+    return render(request, 'Педагогам/attestation.html')
+
+def cabinet_page(request):
+    return render(request, 'Педагогам/cabinet.html')
+
+def application_page(request):
+    return render(request, 'application/application.html')
+
+def antiterrorist_page(request):
+    return render(request, 'information/antiterrorist.html')
+
+def independet_page(request):
+    return render(request, 'information/independet.html')
+
+def corruption_page(request):
+    return render(request, 'information/corruption.html')
+
+def photo_page(request):
+    return render(request, 'Медиа/photo.html')
+
+def video_page(request):
+    return render(request, 'Медиа/video.html')
+
+def quests_page(request):
+    return render(request, 'Медиа/quests.html')
+
+def main_info_page(request):
+    return render(request, 'Сведения об образовательной организации/основные_сведения.html')
+
+def structur_page(request):
+    return render(request, 'Сведения об образовательной организации/Структура и органы управления образовательной организацией.html')
+
+def document_page(request):
+    return render(request, 'Сведения об образовательной организации/документы.html')
+
+def education_page(request):
+    return render(request, 'Сведения об образовательной организации/Образование.html')
+
+def education_standart_page(request):
+    return render(request, 'Сведения об образовательной организации/Образовательные стандарты.html')
+
+def cast_page(request):
+    return render(request, 'Сведения об образовательной организации/Руководство.html')
+
+def security_page(request):
+    return render(request, 'Сведения об образовательной организации/Материально-техническое обеспечение.html')
+
+def scholarship_page(request):
+    return render(request, 'Сведения об образовательной организации/Стипендии и меры поддержки обучающихся.html')
+
+def paid_services_page(request):
+    return render(request, 'Сведения об образовательной организации/Платные образовательные услуги.html')
+
+def financially_page(request):
+    return render(request, 'Сведения об образовательной организации/Финансово-хозяйственная деятельность.html')
+
+def vacant_page(request):
+    return render(request, 'Сведения об образовательной организации/Вакантные места для приема (перевода) обучающихся.html')
+
+def accessible_page(request):
+    return render(request, 'Сведения об образовательной организации/Доступные среды.html')
+
+def organ_page(request):
+    return render(request, 'Сведения об образовательной организации/Предписания органов, осуществляющих государственный контроль (надзор).html')
+
+def cooperation_page(request):
+    return render(request, 'Сведения об образовательной организации/Международное сотрудничество.html')
+
+def distant_page(request):
+    return render(request, 'Сведения об образовательной организации/Дистанционное обучение.html')
+
+def map_page(request):
+    return render(request, 'map.html')
--- djangoProject_UCPK/urls.py
+++ djangoProject_UCPK/urls.py
@@ -19,12 +19,60 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from app1.views import index_page, second_page
+from app1.views import index_page, second_page, first_help_page, occupational_page, teachers_page, professions_page,\
+    forthypointforty_page, news_page, news_new_site_obr_page, medic_and_student_page, greep_page, hygienic_page, \
+    prevention_page, virus_page, contacts_main_page, faq_page, appeals_page, comments_page, deal_page, cabinet_page,\
+    attestation_page, application_page, independet_page, antiterrorist_page, corruption_page, photo_page, video_page, \
+    quests_page, main_info_page, structur_page, document_page, education_page, education_standart_page, cast_page, \
+    security_page, scholarship_page, paid_services_page, financially_page, vacant_page, accessible_page, organ_page,\
+    cooperation_page, distant_page, map_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_page),
     path('second/', second_page),
+    path('first_help/', first_help_page),
+    path('occupational_safety/', occupational_page),
+    path('teachers/', teachers_page),
+    path('professions/', professions_page),
+    path('404/', forthypointforty_page),
+    path('news/', news_page),
+    path('news/news_new_site_obr/', news_new_site_obr_page),
+    path('news/medic_and_student/', medic_and_student_page),
+    path('news/greep/', greep_page),
+    path('news/hygienic/', hygienic_page),
+    path('news/prevention/', prevention_page),
+    path('news/virus/', virus_page),
+    path('contacts/', contacts_main_page),
+    path('contacts/faq/', faq_page),
+    path('contacts/appeals/', appeals_page),
+    path('contacts/comments/', comments_page),
+    path('contacts/appeals/deal', deal_page),
+    path('attestation/', attestation_page),
+    path('cabinet/', cabinet_page),
+    path('application/', application_page),
+    path('independet/', independet_page),
+    path('antiterrorist/', antiterrorist_page),
+    path('corruption/', corruption_page),
+    path('photo/', photo_page),
+    path('video/', video_page),
+    path('quests/', quests_page),
+    path('main_info/', main_info_page),
+    path('structur/', structur_page),
+    path('document/', document_page),
+    path('education/', education_page),
+    path('education_standart/', education_standart_page),
+    path('cast/', cast_page),
+    path('security/', security_page),
+    path('scholarship/', scholarship_page),
+    path('paid_services/', paid_services_page),
+    path('financially/', financially_page),
+    path('vacant/', vacant_page),
+    path('accessible/', accessible_page),
+    path('organ/', organ_page),
+    path('cooperation/', cooperation_page),
+    path('distant/', distant_page),
+    path('map/', map_page),
 ]
 
 if settings.DEBUG:
